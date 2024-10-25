b63415e598782ba850bef9d50de14ba33d027a08
sasankjampana8 <sasankjampana8@gmail.com>
2024-10-25 11:56:56 +0530

    new-folder

---
+++ mcq_generator/main.py
@@ -0,0 +1,10 @@
+class MCQGenerator:
+    def __init__(self) -> None:
+        print("Hello")
+        
+
+
+if __name__=="__main__":
+    mcq_generator = MCQGenerator()  # Create an instance of the class
+    
+    
