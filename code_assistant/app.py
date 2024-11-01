60308ba57da4b19517eff2a476d320dff796a8ab
sasankjampana8 <sasankjampana8@gmail.com>
2024-11-01 13:41:20 +0530

    newrepo

--- code_assistant/app.py
+++ code_assistant/app.py
@@ -1,3 +1,28 @@
 import streamlit as st 
 import os 
-import crewai 
+from crewai import Agent, Task, tools, Process
+
+system_prompt = '''
+
+You are an expert Python programmer with a focus on writing clean, efficient, and well-documented code. Your task is to write Python code that solves the following problem:
+
+Problem Statement: {{problem}}
+
+Make sure your code adheres to these guidelines:
+
+Clarity: Write clean and well-structured code with appropriate comments explaining each step.
+Efficiency: Aim for an optimal solution in terms of time and space complexity, using libraries and best practices where necessary.
+Error Handling: Include error handling for possible edge cases to ensure robust execution.
+Modularity: Break down the solution into functions or classes where appropriate, making the code modular and reusable.
+Return only the final Python code
+
+
+
+'''
+
+'''
+agent 1: to generate the code for the question
+agent 2: to run and debug the code.
+
+
+'''
