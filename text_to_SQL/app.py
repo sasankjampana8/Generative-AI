77bf4f7405eb1215d87ab259a9d5ad7688cf7e2b
sasankjampana8 <sasankjampana8@gmail.com>
2024-11-02 20:30:50 +0530

    smallupdate

--- text_to_SQL/app.py
+++ text_to_SQL/app.py
@@ -18,7 +18,7 @@ def get_db_data(conn, sql):
 
 def create_sql(query):
     model = Ollama(model='llama3')    
-    system_prompt = '''You are an expert at writing PostgreSQL queries. Write a PostgreSQL query for the database: customer, which contains a single table called customer_master with columns: cif_id, customer_name, age, city, occupation, and risk_profile. Add the database name to the column name. for example: select customer.cif_id from table_name;. Only return the SQL query and nothing else.No explanation requried. just the final sql query.
+    system_prompt = '''You are an expert at writing PostgreSQL queries. Write a PostgreSQL query for the database: customer, which contains a single table called customer_master with columns: cif_id, customer_name, age, city, occupation, and risk_profile. Add the database name to the column_name. for example: select customer.cif_id from table_name;. Only return the SQL query and nothing else.No explanation requried. just the final sql query.
 '''
     request = f'{system_prompt}. write the sql query for the following {query}. just return the SQL query as output and nothing else'
     result = model.invoke(request)
@@ -29,7 +29,7 @@ def create_sql(query):
 
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Text to SQL", page_icon=":balloon:")
+    st.set_page_config(page_title="Chat with MyDataBase", page_icon=":speech_balloon:")
     conn =  connect_to_db()
     user_input = st.text_input(label="Enter your query:")
     if user_input:
