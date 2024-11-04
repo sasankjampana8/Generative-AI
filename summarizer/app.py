9dc8b2179f5353c12bd4d8f5985c90049d49ff0c
sasankjampana8 <sasankjampana8@gmail.com>
2024-11-04 11:21:14 +0530

    newfiles

---
+++ summarizer/app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import os 
+from dotenv import load_dotenv
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.embeddings import OllamaEmbeddings, HuggingFaceInstructEmbeddings
+from langchain.chains import LLMChain
+
+
+'''
+System prompt: Research Paper Summarizer
+
+
+
+'''
