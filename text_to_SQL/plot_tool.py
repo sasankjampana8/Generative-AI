af2987924fa71061ac9d8b341ec581f5334e50a1
sasankjampana8 <sasankjampana8@gmail.com>
2024-11-30 10:35:52 +0530

    newfile

---
+++ text_to_SQL/plot_tool.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as pyplot
+import os
+import numpy as np
+import pandas as pd
+
+
+
+
+
+def main():
+    '''
+    After extracting data from the DB, this function will be used to create a plot of the data (if necessary).
+    
+    1. Line Chart
+    2. Bar Chart
+    3. Pie Chart
+    4. Histogram
+    5. Scatter Plot
+    ...
+    
+    input: we expect the data in a JSON format. 
+    
+    output: We save the Plot in a open bucket or publicly accessible storage. return the plot path
+    '''
+    #plot a new figure
+    pass
+    
+if __name__ == "__main__":
+    main()
